b3e561b3bf92fe0b44e12c0d187044cf706b87e7
root <root@raspberrypi.(none)>
2013-03-09 12:42:14 -0500

    Write to a sqlite3 database file.

--- temp.py
+++ temp.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
+
 import datetime
 import sys
+
+import sqlite3
+
+import lcddriver
+
+
+def get_dbhandle():
+    con = None
+    con = sqlite3.connect('/var/lib/rpi-datalogger/database.db')
+    return con
+
 def get_temp(file_link):
    file = open(file_link)
    text = file.read()
@@ -10,29 +22,27 @@ def get_temp(file_link):
      sys.exit(0)
    secondline = text.split("\n")[1]
    temp = secondline.split(" ")[9]
-
-
    temp = round((int(temp[2:])/1000.0)*1.8+32,2)
    return temp
 
+def main():
+    inside = get_temp("/sys/bus/w1/devices/28-000003d37411/w1_slave")
+    outside = get_temp("/sys/bus/w1/devices/28-00000428d743/w1_slave")
 
-inside = get_temp("/sys/bus/w1/devices/28-000003d37411/w1_slave")
-outside = get_temp("/sys/bus/w1/devices/28-00000428d743/w1_slave")
+    con = get_dbhandle()
+    cur = con.cursor()
+    sql = 'insert into trends (timestamp, insidetemp_f, outsidetemp_f) values ("%s", %s, %s)' % ( datetime.datetime.now(), inside, outside)
 
-print str(datetime.datetime.now())[0:19],",",inside,",",outside
-
-import lcddriver
-import sys
-
-lcd = lcddriver.lcd(init=False)
-
-
-
-
-lcd.lcd_display_string("Inside : " +str(inside)+' F  ',2)
-lcd.lcd_display_string("Outside: " + str(outside) + " F  ", 3)
-lcd.lcd_display_string(str(datetime.datetime.now())[0:18],4)
-
+    print sql 
+    cur.execute(sql)
+    con.commit()
+    print str(datetime.datetime.now())[0:19],",",inside,",",outside
+    lcd = lcddriver.lcd(init=False)
+    lcd.lcd_display_string("Inside : " +str(inside)+' F  ',2)
+    lcd.lcd_display_string("Outside: " + str(outside) + " F  ", 3)
+    lcd.lcd_display_string(str(datetime.datetime.now())[0:18],4)
 
 
+if __name__ == "__main__":
+    main()
 
