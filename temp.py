546b9678b65a6454f7171cc1d28afdb2f3d4ff11
sbrinkerhoff <git@vtwireless.com>
2013-03-09 21:05:00 -0500

    Cleanup from Pylink

--- temp.py
+++ temp.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+""" Simple Python datalogger for the Raspberry PI """
+
 import datetime
 import sys
 
@@ -9,40 +11,52 @@ from i2c_lib import lcddriver
 
 
 def get_dbhandle():
+    """ Return a database handle to the SQLite file """
     con = None
     con = sqlite3.connect('/var/lib/rpi-datalogger/database.db')
     return con
 
 def get_temp(file_link):
-   file = open(file_link)
-   text = file.read()
-   file.close()
-   firstline = text.split("\n")[0]
-   if not 'YES' in firstline:
-     sys.exit(0)
-   secondline = text.split("\n")[1]
-   temp = secondline.split(" ")[9]
-   temp = round((int(temp[2:])/1000.0)*1.8+32,2)
-   return temp
+    """ Given a file_link to a dallas one-wire device return the
+        temperature in fahrenheit  """
+
+    with open(file_link) as sensor: 
+        text = sensor.read()
+
+    firstline = text.split("\n")[0]
+    secondline = text.split("\n")[1]
+    if not 'YES' in firstline:
+        sys.exit(0)
+    temp = secondline.split(" ")[9]
+
+    # Sensor reads in celsius, C * 1.8 + 32 = F
+    temp = round((int(temp[2:])/1000.0)*1.8+32, 2)
+    return temp
 
 def main():
+    """ Where the magic happens. """
+
     inside = get_temp("/sys/bus/w1/devices/28-000003d37411/w1_slave")
     outside = get_temp("/sys/bus/w1/devices/28-000003ea8a14/w1_slave")
 
+    curdate = str(datetime.datetime.now())[0:19]
+
     con = get_dbhandle()
     cur = con.cursor()
-    sql = 'insert into trends (timestamp, insidetemp_f, outsidetemp_f) values ("%s", %s, %s)' % ( str(datetime.datetime.now())[0:19], inside, outside)
+    sql = 'insert into trends (timestamp, insidetemp_f, outsidetemp_f) \
+           values ("%s", %s, %s)' % (curdate, inside, outside)
 
-
-    print sql 
     cur.execute(sql)
     con.commit()
-    print str(datetime.datetime.now())[0:19],",",inside,",",outside
-    lcd = lcddriver.lcd(init=False)
-    lcd.lcd_display_string("Inside : " +str(inside)+' F  ',2)
-    lcd.lcd_display_string("Outside: " + str(outside) + " F  ", 3)
-    lcd.lcd_display_string(str(datetime.datetime.now())[0:19],4)
 
+    curdate = str(datetime.datetime.now())[0:19]
+
+    print curdate, ",", inside, ",", outside
+
+    lcd = lcddriver.lcd(init=False)
+    lcd.lcd_display_string("Inside : " +str(inside)+' F  ', 2)
+    lcd.lcd_display_string("Outside: " + str(outside) + " F  ", 3)
+    lcd.lcd_display_string(curdate, 4)
 
 if __name__ == "__main__":
     main()
